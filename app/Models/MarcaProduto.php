ac71512762c7387717c61aa7cdad262dafbc2e7e
Gabriel Barbosa <gabrielbarbosadev2022@gmail.com>
2023-12-07 13:41:51 -0300

    Back end do CRUD da tabela marca-produtos criado (model, migration, controller, route)

---
+++ app/Models/MarcaProduto.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MarcaProduto extends Model
+{
+    use HasFactory;
+
+    protected $table = "marca_produtos";
+
+    protected $fillable = [
+        'nome_marca',
+        'descricao_marca'
+    ];
+}
